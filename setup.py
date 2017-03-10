888c1bd7e8c28894a965110420cd9230ed3c8e80
Jose Antonio Perdiguero <perdy.hh@gmail.com>
2017-03-10 14:49:02 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ _KEYWORDS = ' '.join([
 
 setup(
     name='clinner',
-    version='0.3.1',
+    version='0.3.2',
     description='Command Line Interface builder that helps creating an entry point for your application.',
     long_description=_LONG_DESCRIPTION,
     author='José Antonio Perdiguero López',
