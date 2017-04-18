fb2bdbbb9a995f58c888a501d2fb0f949d4227e0
Jose Perdiguero <perdy.hh@gmail.com>
2017-04-18 16:41:38 +0200

    Bump version: 1.1.0 → 1.1.1

--- README.rst
+++ README.rst
@@ -3,7 +3,7 @@ Clinner
 *******
 |build-status| |coverage| |version|
 
-:Version: 1.1.0
+:Version: 1.1.1
 :Status: Production/Stable
 :Author: José Antonio Perdiguero López
 
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ _KEYWORDS = ' '.join([
 
 setup(
     name='clinner',
-    version='1.1.0',
+    version='1.1.1',
     description='Command Line Interface builder that helps creating an entry point for your application.',
     long_description=_LONG_DESCRIPTION,
     author='José Antonio Perdiguero López',
